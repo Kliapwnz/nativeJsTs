3954daf3d5159a2fdda52d6e55cd603f7d4f29d6
Evgeniy Klimovch <kliasanik@gmail.com>
2024-01-04 15:37:14 +0300

    onClick

--- src/components/CallBacks/CallBack.tsx
+++ src/components/CallBacks/CallBack.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export const CallBack = () => {
+    const deleteUser = () => {
+        alert("delete please bro")
+    }
+    const saveUser = () => {
+        alert("save please bro")
+    }
+
+
     return (
         <div>
             Hello
+            <button onClick={deleteUser}>delete</button>
+            <button onClick={saveUser}>save</button>
         </div>
     );
 };
